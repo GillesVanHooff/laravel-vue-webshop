b070c43dec2d6515d8b3825bad165a013a2efa9a
Gilles Van Hooff <vanhooff0@gmail.com>
2022-11-22 22:03:23 +0100

    Also add routing to reset password page

--- backend/src/views/RequestPassword.vue
+++ backend/src/views/RequestPassword.vue
@@ -15,7 +15,7 @@
                 <h2
                     class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
                 >
-                    Sign in to your account
+                    Request a new password
                 </h2>
             </div>
             <form class="mt-8 space-y-6" action="#" method="POST">
@@ -35,40 +35,14 @@
                             placeholder="Email address"
                         />
                     </div>
-                    <div>
-                        <label for="password" class="sr-only">Password</label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autocomplete="current-password"
-                            required=""
-                            class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Password"
-                        />
-                    </div>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input
-                            id="remember-me"
-                            name="remember-me"
-                            type="checkbox"
-                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                        />
-                        <label
-                            for="remember-me"
-                            class="ml-2 block text-sm text-gray-900"
-                            >Remember me</label
-                        >
-                    </div>
-
                     <div class="text-sm">
                         <a
-                            href="/request-password"
+                            href="/login"
                             class="font-medium text-indigo-600 hover:text-indigo-500"
-                            >Forgot your password?</a
+                            >Remember your password?</a
                         >
                     </div>
                 </div>
@@ -86,7 +60,7 @@
                                 aria-hidden="true"
                             />
                         </span>
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
