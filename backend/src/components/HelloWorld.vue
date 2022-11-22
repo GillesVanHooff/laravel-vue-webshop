cd917f93ffbdf0fff97dfc8e20fe4ece4b09863e
Gilles Van Hooff <vanhooff0@gmail.com>
2022-11-22 21:49:57 +0100

    Use template for login form from tailwindui.com and install some dev dependecies we'll need later

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed  } from 'vue'
+import store from '../store'
+
+const test = computed(() => store.state.some_value)
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="bg-red-500">{{ test }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
